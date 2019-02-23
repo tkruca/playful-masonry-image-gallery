a93f98d86d0d1e3acd866de8e3c6a0b700a81c98
Mirza <mirzahodzic88@gmail.com>
2019-02-23 22:17:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Playful Masonry Image Gallery
+
+A responsive example of masonry image gallery using Masonry JavaScript grid layout library, GSAP's TweenMax and ScrollMagic. 
+
+![Playful Masonry Image Gallery](https://coidea.website/api/wp-content/uploads/2019/02/playful-masonry-image-gallery.png)
+
+[Article on COIDEA](https://coidea.website/categories/challenges/playful-masonry-image-gallery/)
+
+[Demo](https://coidea.website/demos/playful-masonry-image-gallery/)
+
+## Credits
+- The inspiration for effects and Layout comes from [Bakhtiyar Sattarov](https://dribbble.com/shots/6012787-Craftwork-Design-reDesign)
+- [Masonry](https://masonry.desandro.com/) by David DeSandro
+- [TweenMax](https://greensock.com/tweenmax) and [ScrollToPlugin](https://greensock.com/ScrollToPlugin) by Greensock
+- [ScrollMagic](http://scrollmagic.io/) and [animation.gsap](http://scrollmagic.io/docs/animation.GSAP.html) by Jan Paepke
+- [imagesLoaded](https://imagesloaded.desandro.com/) by David DeSandro
+- [Images](https://unsplash.com/collections/1886495/through-a-rainy-window) by Unsplash.com
+- Font [Poppins](https://fonts.google.com/specimen/Poppins) by Google Fonts
+- Simple Loader by [loading.io](https://loading.io/css/)
+- Umbrella icon made by [Vectors Market](https://www.flaticon.com/authors/vectors-market) from [Flaticon](https://www.flaticon.com/) are licensed by [CC 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+## License
+This resource can be used freely if integrated or build upon in personal or commercial projects such as websites, web apps and web templates intended for sale. It is not allowed to take the resource "as-is" and sell it, redistribute, re-publish it, or sell "pluginized" versions of it. Free plugins built using this resource should have a visible mention and link to the original work. Always consider the licenses of all included libraries, scripts and images used.
+
+## Misc
+
+Follow Mirza Hodzic: [GitHub](https://github.com/MirzaHodzic), [Instagram](https://www.instagram.com/mirza__h__/), [Twitter](https://twitter.com/mirzahodzic88)
+
+Follow COIDEA: [GitHub](https://github.com/COIDEAwebsite), [Instagram](https://www.instagram.com/coidea.website/), [Facebook](https://www.facebook.com/Coidea), [Twitter](https://twitter.com/CoideaW)
+
+[© COIDEA 2018](https://coidea.website)
---
+++ assets/js/demo.js
@@ -0,0 +1,132 @@
+/**
+ * demo.js
+ * https://coidea.website
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2018, COIDEA
+ * https://coidea.website
+ */
+
+  // masonry ====================
+  var $grid = $('.grid').masonry({
+    itemSelector: '.masonry__item'
+  });
+  $grid.imagesLoaded()
+    .progress( function() {
+      $grid.masonry();
+    })
+    .done( function() {
+      $('.loader').addClass('is-loaded');
+    });
+
+  // smooth scroll ====================
+  $(function(){
+    
+    var $window = $(window),
+        scrollTime = 0.8,
+        scrollDistance = 240;
+      
+    $window.on("mousewheel DOMMouseScroll touchmove", function(event){
+      
+      event.preventDefault();	  
+      var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3,
+          scrollTop = $window.scrollTop(),
+          finalScroll = scrollTop - parseInt(delta*scrollDistance);
+        
+      TweenMax.to($window, scrollTime, {
+        scrollTo : { y: finalScroll, autoKill:true },
+        ease: Power4.easeOut,
+        autoKill: true,
+        overwrite: 5							
+      });     
+    });
+  });
+
+  // main timeline
+  $(".masonry__item").each(function(index, element){
+    var timeline = new TimelineMax();
+    timeline.fromTo($(this), .35, { autoAlpha: 0.15 }, { autoAlpha: 1, ease:Power4.easeInOut })
+            .set($(this), { className: "+=active" })
+            .staggerTo('.masonry__item', .35, { scale: 0.85, opacity: 0.05, ease:Power4.easeInOut }, "-=.05", "-=.30")
+            .to($(this), .35, { zIndex: 15, rotation: 15, scale: 1.35, ease: Back.easeOut.config(1.7) })
+            .fromTo($(this).find('.background'), .35, { autoAlpha: 0, rotation: -15, scale: 1.5 }, { autoAlpha: 0.15, rotation: 0, scale: 1.75, ease:Power4.easeInOut }, "-=.35")
+            .fromTo($(this).find('h2'), .35, { autoAlpha: 0.15, rotation: 0 }, { autoAlpha: 1, rotation: 0, ease:Power4.easeInOut }, "-=.35")
+            .set($(this).find('.detail'), { className: "+=detail-active" })
+            .fromTo($(this).find('.detail'), .35, { autoAlpha: 0, rotation: 0 }, { autoAlpha: 1, rotation: 0, ease:Power4.easeInOut }, "-=.35")
+            .paused(true)
+            .reversed(true);
+    this.animation = timeline;
+    // trigger
+    $('.close-detail-view').on('click', function() {
+      $(this).removeClass('active');
+      timeline.reverse();
+    });
+  });
+
+  $('.masonry__item').on('click', function() {
+    if( $('.close-detail-view').hasClass('active') ) {
+      $('.close-detail-view').removeClass('active');
+    } else {
+      $('.close-detail-view').addClass('active');
+    }
+    this.animation.reversed() ? this.animation.play() : this.animation.reverse();
+  });
+
+  // animation on scroll
+  var controller = new ScrollMagic.Controller(),
+      items = Array.prototype.slice.call(document.querySelectorAll(".masonry__item")),
+      self = this;
+      items.forEach(function(self) {
+        var sceneTimelineIn = new TimelineMax(),
+            sceneTimelineOut = new TimelineMax();
+        sceneTimelineIn
+          .fromTo(self, .35, {y: 120}, {y: 0}, self);
+        sceneTimelineOut
+          .fromTo(self, .35, {y: 0}, {y: -120}, self);
+
+        var sceneIn = new ScrollMagic.Scene({
+            triggerElement: self,
+            triggerHook: "onEnter",
+            duration: $(window).height() / 2,
+            offset: -60
+        })
+        .setTween(sceneTimelineIn)
+        .setClassToggle(self, "centered")
+        .addTo(controller);
+        
+        var sceneOut = new ScrollMagic.Scene({
+            triggerElement: self,
+            triggerHook: "onLeave",
+            duration: $(window).height() / 2,
+            offset: 60
+        })
+        .setTween(sceneTimelineOut)
+        .setClassToggle(self, "centered")
+        .addTo(controller);
+  });
+
+  // hover on active
+  $('.masonry__item').each(function(i, el) {
+    
+    var hoverTimeline = new TimelineMax({
+      paused: true
+    });
+
+    hoverTimeline.fromTo($(this).find('.circle2 > span'), .35, { x: 0 }, { x: 100, ease:Power4.easeInOut })
+                  .fromTo($(this).find('.circle1'), .35, { x: 0 }, { x: 24, ease:Power4.easeInOut }, "-=.20");
+    
+    
+    $(document).on("mouseenter", ".detail-active", function() {
+      hoverTimeline.play();
+    });
+
+    $(document).on("mouseleave", ".detail-active", function() {
+      hoverTimeline.reverse();
+    });
+
+  });
+
+  // fast fix for resize window and refresh view, attention: not use in production!
+  window.onresize = function(){ location.reload(); }
